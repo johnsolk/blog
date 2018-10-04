ad0de33368b19a66f55a346956ecfa7a1a191506
Lisa Johnson <ljcohen@ucdavis.edu>
2018-10-04 12:29:48 -0700

    switched to bootstrap3

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,111 +2,50 @@
 # -*- coding: utf-8 -*- #
 import markdown
 
-AUTHOR = 'Lisa Johnson'
-SITENAME = 'monsteRbashSeq'
+AUTHOR = u'Lisa Johnson'
+SITENAME = u'monsteRbashSeq'
 SITEURL = 'https://ljcohen.github.io/blog/'
 TIMEZONE = 'America/Los_Angeles'
-DEFAULT_LANG = 'en'
-#DEFAULT_DATE = 'fs'
-#DEFAULT_DATE_FORMAT = '%d %b %Y'
+DEFAULT_LANG = u'en'
 
 PATH = 'content'
+
 OUTPUT_PATH = 'output'
 
+CUSTOM_CSS = 'static/custom.css'
 
 THEME = './theme'
-# https://github.com/arulrajnet/attila
+# https://github.com/getpelican/pelican-themes/tree/master/pelican-bootstrap3
+
 LINKS = [('name','url')]
 
-HEADER_COVER = './img/sanomacoast.jpg'
-#GITHUB_URL = 'https://github.com/ljcohen'
-#TWITTER_USERNAME = 'monsterbashseq'
-DISQUS_SITENAME = 'https-ljcohen-github-io-blog'
+DISPLAY_CATEGORIES_ON_MENU = True
 
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
-#SOCIAL = [('twitter','https://twitter.com/monsterbashseq')]
+DISPLAY_PAGES_ON_MENU = True
+
+BOOTSTRAP_THEME = 'simplex'
+
+BOOTSTRAP_NAVBAR_INVERSE = True
+
 
 SOCIAL = (('twitter', 'https://twitter.com/monsterbashseq'),
           ('github', 'https://github.com/ljcohen'),
           ('flickr','https://www.flickr.com/155690017@N06/'),
+          ('linkedin','https://www.linkedin.com/in/johnsolk/'),
           ('envelope','mailto:ljcohen@ucdavis.edu'))
 
-# Post and Pages path
-#ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
-#ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
-#PAGE_URL = 'pages/{slug}/'
-#PAGE_SAVE_AS = 'pages/{slug}/index.html'
-#YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
-#MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+DISQUS_SITENAME = "johnson_lk"
 
-# Tags and Category path
-#CATEGORY_URL = 'category/{slug}'
-#CATEGORY_SAVE_AS = 'category/{slug}/index.html'
-#CATEGORIES_SAVE_AS = 'catgegories.html'
-#TAG_URL = 'tag/{slug}'
-#TAG_SAVE_AS = 'tag/{slug}/index.html'
-#TAGS_SAVE_AS = 'tags.html'
-
-# Author
-#AUTHOR_URL = 'author/{slug}'
-#AUTHOR_SAVE_AS = 'author/{slug}/index.html'
-#AUTHORS_SAVE_AS = 'authors.html'
-
-### Plugins
-
-#PLUGIN_PATHS = [
-#  'pelican-plugins'
-#]
-
-#PLUGINS = [
-#  'sitemap',
-#  'neighbors',
-#  'assets'
-#]
-
-# Sitemap
-#SITEMAP = {
-#    'format': 'xml',
-#    'priorities': {
-#        'articles': 0.5,
-#        'indexes': 0.5,
-#        'pages': 0.5
-#    },
-#    'changefreqs': {
-#        'articles': 'monthly',
-#        'indexes': 'daily',
-#        'pages': 'monthly'
-#    }
-#}
-
-# Comments
-DISQUS_SITENAME = "attilademo"
-
-# Analytics
-GOOGLE_ANALYTICS = "UA-3546274-12"
+#GOOGLE_ANALYTICS = ""
 
 
 ### Theme specific settings
-
-HEADER_COVER = 'img/sanomacoast.jpg'
-
-COLOR_SCHEME_CSS = 'github.css'
-
-CSS_OVERRIDE = ['css/myblog.css']
-
-AUTHORS_BIO = {
-  "zutrinken": {
-    "name": "Zutrinken",
-    "cover": "https://casper.ghost.org/v1.0.0/images/team.jpg",
-    "image": "assets/images/avatar.png",
-    "website": "http://blog.arulraj.net",
-    "linkedin": "unavailable",
-    "github": "arulrajnet",
-    "location": "Chennai",
-    "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
-  }
-}
-
-# Custom Header
-
-#HEADER_COVERS_BY_TAG = {'cupcake': 'assets/images/rainbow_cupcake_cover.png', 'general':'https://casper.ghost.org/v1.0.0/images/writing.jpg'}
+BANNER = './img/sanomacoast.jpg'
+BANNER_SUBTITLE = 'Lisa K. Johnson'
+BANNER_ALL_PAGES = True
+DISPLAY_TAGS_ON_SIDEBAR = True
+TAGS_URL = 'tags.html'
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+DISPLAY_ARCHIVE_ON_SIDEBAR = True
